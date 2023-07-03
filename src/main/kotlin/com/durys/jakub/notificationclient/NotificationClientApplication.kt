eec4a29d41d7180b9513dbab604654c646352f39
kubavation <37585627+kubavation@users.noreply.github.com>
2023-07-03 18:09:35 +0200

    init

---
+++ src/main/kotlin/com/durys/jakub/notificationclient/NotificationClientApplication.kt
@@ -0,0 +1,11 @@
+package com.durys.jakub.notificationclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/durys/jakub/notificationclient/NotificationClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.notificationclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
